8d434034a0165b7180cc1f07902af05457cbd18c
ANDRmark <Andr98m@gmail.com>
2017-12-21 15:04:53 +0200

    labs for reports ess (2.3.19(3)) and adv (3.1.8.1)

---
+++ practiceLabsForReports/Lab_2_3_19(3).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int power;
+	unsigned long long a = 1;
+	cout << "input power of 2 (number from 0 to 63) ";
+	cin >> power;
+	if (power < 0 || power > 63) cout << "power is out of range" << endl;
+	else 
+	{
+		for (int i = 0; i < power; i++)
+			a *= 2;
+		cout << a << endl;
+	}
+	return 0;
+}
