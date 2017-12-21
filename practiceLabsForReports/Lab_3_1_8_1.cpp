76eb69956b352220f112ebc2243036bcd12efcb7
ANDRmark <Andr98m@gmail.com>
2017-12-21 16:22:17 +0200

    lab2 updated

--- practiceLabsForReports/Lab_3_1_8_1.cpp
+++ practiceLabsForReports/Lab_3_1_8_1.cpp
@@ -17,7 +17,7 @@ int main()
 	vector<int> original_values =
 	{ 210, 314, 202, 110, 211, 302, 358, 229, 260, 411, 412, 311, 373, 483 };
 	vector<int> noisy_values =
-	{ 211, 324, 203, 113, 227, 320, 340, 210, 239, 411, 412, 333, 371, 601 };
+	{ 211, 324, 203, 113, 227, 320, 340, 210, 239, 411, 412, 333, 371, 501 };
 
 	pair<vector<int>::iterator, vector<int>::iterator> mypair;
 	limit = 15;
@@ -26,7 +26,7 @@ int main()
 	{
 		mypair = mismatch(original_values.begin(), original_values.end(), noisy_values.begin(), mypredicate);
 		if (mypair.first == original_values.end() || mypair.second == noisy_values.end()) break;
-		cout << "First difference greater than " << limit << " is found:" << *mypair.first << " - " << *mypair.second << '\n';
+		cout << "First difference greater than " << limit << " is found: " << *mypair.first << " - " << *mypair.second << '\n';
 		limit++;
 		if (limit == 400) isMismatch = false;
 	}
